1493f38a5071af71ec985c2c34893b6dd662170c
Maria Novokovich <kataskin@yandex.ru>
2016-03-26 20:27:44 +0300

    Второе задание

--- build/js/resizer.js
+++ build/js/resizer.js
@@ -83,21 +83,6 @@
       // Очистка изображения.
       this._ctx.clearRect(0, 0, this._container.width, this._container.height);
 
-      // Параметры линии.
-      // NB! Такие параметры сохраняются на время всего процесса отрисовки
-      // canvas'a поэтому важно вовремя поменять их, если нужно начать отрисовку
-      // чего-либо с другой обводкой.
-
-      // Толщина линии.
-      this._ctx.lineWidth = 6;
-      // Цвет обводки.
-      this._ctx.strokeStyle = '#ffe753';
-      // Размер штрихов. Первый элемент массива задает длину штриха, второй
-      // расстояние между соседними штрихами.
-      this._ctx.setLineDash([15, 10]);
-      // Смещение первого штриха от начала линии.
-      this._ctx.lineDashOffset = 7;
-
       // Сохранение состояния канваса.
       // Подробней см. строку 132.
       this._ctx.save();
@@ -112,13 +97,53 @@
       // Координаты задаются от центра холста.
       this._ctx.drawImage(this._image, displX, displY);
 
+      // Сохраним значение толщины рамки заранее, так как оно нам понадобится
+      // для расчетов параметров затеняющего слоя.
+      var borderLineWidth = 6;
+
+      this._ctx.save();
+      this._ctx.beginPath();
+      this._ctx.rect(displX, displY, this._container.width, this._container.height);
+      this._ctx.rect(
+          (-this._resizeConstraint.side / 2) - borderLineWidth,
+          (-this._resizeConstraint.side / 2) - borderLineWidth,
+          this._resizeConstraint.side + 2 * borderLineWidth,
+          this._resizeConstraint.side + 2 * borderLineWidth);
+      this._ctx.closePath();
+      this._ctx.clip('evenodd');
+      this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+      this._ctx.fillRect(displX, displY, this._container.width, this._container.height);
+      this._ctx.restore();
+
+      // Параметры линии.
+      // NB! Такие параметры сохраняются на время всего процесса отрисовки
+      // canvas'a поэтому важно вовремя поменять их, если нужно начать отрисовку
+      // чего-либо с другой обводкой.
+
+      // Толщина линии.
+      this._ctx.lineWidth = borderLineWidth;
+      // Цвет обводки.
+      this._ctx.strokeStyle = '#ffe753';
+      // Размер штрихов. Первый элемент массива задает длину штриха, второй
+      // расстояние между соседними штрихами.
+      this._ctx.setLineDash([15, 10]);
+      // Смещение первого штриха от начала линии.
+      this._ctx.lineDashOffset = 7;
+
       // Отрисовка прямоугольника, обозначающего область изображения после
       // кадрирования. Координаты задаются от центра.
       this._ctx.strokeRect(
-          (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
-          (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
-          this._resizeConstraint.side - this._ctx.lineWidth / 2,
-          this._resizeConstraint.side - this._ctx.lineWidth / 2);
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
+        (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
+        this._resizeConstraint.side + this._ctx.lineWidth,
+        this._resizeConstraint.side + this._ctx.lineWidth);
+
+      var text = this._image.naturalWidth + ' x ' + this._image.naturalHeight;
+      this._ctx.fillStyle = '#ffffff';
+      this._ctx.font = '14px Arial';
+      this._ctx.textAlign = 'center';
+      this._ctx.textBaseline = 'bottom';
+      this._ctx.fillText(text, 0, -this._resizeConstraint.side / 2 - borderLineWidth * 2);
 
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
