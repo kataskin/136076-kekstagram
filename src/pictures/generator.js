fcffd99eac8518e2aa02c0c8dedc001c2443512e
kataskin <apps.net@ya.ru>
2016-04-20 13:02:06 +0300

    17 Первый класс

---
+++ src/pictures/generator.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// шаблон
+var templates = document.getElementById('picture-template');
+var template = ('content' in templates) ?
+  templates.content.querySelector('.picture') :
+  templates.querySelector('.picture');
+
+var create = function(picture) {
+  var element = template.cloneNode(true);
+  var img = element.querySelector('img');
+  var image = new Image();
+  image.onload = function() {
+    img.setAttribute('src', picture.url);
+  };
+  image.onerror = function() {
+    element.classList.add('picture-load-failure');
+  };
+  image.src = picture.url;
+  element.querySelector('.picture-comments').textContent = picture.comments;
+  element.querySelector('.picture-likes').textContent = picture.likes;
+  return element;
+};
+
+module.exports = {
+  create: create
+};
---
+++ src/pictures/photo.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var generator = require('./generator');
+var gallery = require('../gallery');
+var utils = require('../utils');
+
+var Photo = function(data) {
+  this.data = data;
+  this.element = generator.create(data);
+  var index = this.data.index;
+  this.clickHandler = function(evt) {
+    evt.preventDefault();
+    gallery.show(index);
+  };
+  this.keyHandler = function(evt) {
+    if (utils.isActivationEvent(evt)) {
+      evt.preventDefault();
+      gallery.show(index);
+    }
+  };
+  this.remove = function() {
+    this.element.removeEventListener('click', this.clickHandler);
+    this.element.removeEventListener('keydown', this.keyHandler);
+    this.element.parentNode.removeChild(this.element);
+  };
+  this.element.addEventListener('click', this.clickHandler);
+  this.element.addEventListener('keydown', this.keyHandler);
+};
+
+module.exports = Photo;
