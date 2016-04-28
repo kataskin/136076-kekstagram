10b0b9895f2a7a4609d09fc8b42c9f3ca1221594
kataskin <kataskin@yandex.ru>
2016-04-28 10:18:13 +0300

    21 Контексты и прототипы

--- src/pictures/photo.js
+++ src/pictures/photo.js
@@ -7,24 +7,28 @@ var gallery = require('../gallery');
 var Photo = function(data) {
   this.data = data;
   this.element = generator.create(data);
-  var index = this.data.index;
-  this.clickHandler = function(evt) {
-    evt.preventDefault();
-    gallery.show(index);
-  };
-  this.keyHandler = function(evt) {
-    if (utils.isActivationEvent(evt)) {
-      evt.preventDefault();
-      gallery.show(index);
-    }
-  };
-  this.remove = function() {
-    this.element.removeEventListener('click', this.clickHandler);
-    this.element.removeEventListener('keydown', this.keyHandler);
-    this.element.parentNode.removeChild(this.element);
-  };
+  this.index = this.data.index;
+  this.clickHandler = this.clickHandler.bind(this);
   this.element.addEventListener('click', this.clickHandler);
+  this.keyHandler = this.keyHandler.bind(this);
   this.element.addEventListener('keydown', this.keyHandler);
+};
+
+Photo.prototype.clickHandler = function(evt) {
+  evt.preventDefault();
+  gallery.show(this.index);
+};
+Photo.prototype.keyHandler = function(evt) {
+  if (utils.isActivationEvent(evt)) {
+    evt.preventDefault();
+    gallery.show(this.index);
+  }
+};
+
+Photo.prototype.remove = function() {
+  this.element.removeEventListener('click', this.clickHandler);
+  this.element.removeEventListener('keydown', this.keyHandler);
+  this.element.parentNode.removeChild(this.element);
 };
 
 module.exports = Photo;
