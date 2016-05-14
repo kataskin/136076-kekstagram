777fdb357bf0fc195f2c2d20a5f0433d0d9852c3
kataskin <kataskin@yandex.ru>
2016-05-14 21:18:30 +0300

    Убран лишний (насколько я понимаю) обработчик Photo.clickHandler

--- src/pictures/photo.js
+++ src/pictures/photo.js
@@ -6,8 +6,6 @@ var utils = require('../utils');
 var Photo = function(data) {
   this.data = data;
   this.element = generator.create(data);
-  this.clickHandler = this.clickHandler.bind(this);
-  this.element.addEventListener('click', this.clickHandler);
   this.keyHandler = this.keyHandler.bind(this);
   this.element.addEventListener('keydown', this.keyHandler);
 };
@@ -16,9 +14,6 @@ Photo.prototype.showGallery = function() {
   location.hash = 'photo/' + this.data.url;
 };
 
-Photo.prototype.clickHandler = function(evt) {
-  evt.preventDefault();
-};
 Photo.prototype.keyHandler = function(evt) {
   if (utils.isActivationEvent(evt)) {
     evt.preventDefault();
